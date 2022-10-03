c99b86dd9a5bf823539eddaeed3b925dc5bdfdf2
Filipp Goriainov <barrios1991phil@gmail.com>
2022-10-03 20:34:36 +0400

    make better experience

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -1,18 +1,20 @@
 import { Input } from 'antd';
 import './Input.css';
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
+// import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 export default function Search(props) {
-    const { onChange, isLoaded } = props;
-    const inputRef = useRef();
-    useEffect(() => {
-        if (!isLoaded) inputRef.current.focus();
-        else inputRef.current.blur();
-    });
+    const { onChange } = props;
+    // const { onChange, isLoaded } = props;
+    // const inputRef = useRef();
+    // useEffect(() => {
+    //     if (!isLoaded) inputRef.current.focus();
+    //     else inputRef.current.blur();
+    // });
     return (
         <Input
-            ref={inputRef}
+            // ref={inputRef}
             onChange={onChange}
             className="input"
             placeholder="Try typing..."
@@ -22,10 +24,10 @@ export default function Search(props) {
 
 Search.propTypes = {
     onChange: PropTypes.func,
-    isLoaded: PropTypes.bool,
+    // isLoaded: PropTypes.bool,
 };
 
 Search.defaultProps = {
     onChange: () => {},
-    isLoaded: false,
+    // isLoaded: false,
 };
